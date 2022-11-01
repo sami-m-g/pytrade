c1ab03d87562776327d468606b8478ae313365a4
sami-m-g <sami.mg@outlook.com>
2022-11-01 18:44:16 +0200

    pytrade/views.py: added HTTP method handling for /trade route

--- pytrade/views.py
+++ pytrade/views.py
@@ -10,12 +10,16 @@ def index():
 
 @app.route("/trade/", methods=["GET", "POST"])
 def trade():
-    Trader(
-        williams_short_lookback=int(request.form["williams_short_lookback"]),
-        williams_short_overbought=int(request.form["williams_short_overbought"]),
-        williams_short_oversold=int(request.form["williams_short_oversold"]),
-        williams_long_lookback=int(request.form["williams_long_lookback"]),
-        williams_long_overbought=int(request.form["williams_long_overbought"]),
-        williams_long_oversold=int(request.form["williams_long_oversold"]),
-    ).trade()
-    return "Done"
+    if request.method == "GET":
+        return "Processing..."
+
+    if request.method == "Post":
+        Trader(
+            williams_short_lookback=int(request.form["williams_short_lookback"]),
+            williams_short_overbought=int(request.form["williams_short_overbought"]),
+            williams_short_oversold=int(request.form["williams_short_oversold"]),
+            williams_long_lookback=int(request.form["williams_long_lookback"]),
+            williams_long_overbought=int(request.form["williams_long_overbought"]),
+            williams_long_oversold=int(request.form["williams_long_oversold"]),
+        ).trade()
+        return "Processing done!"
