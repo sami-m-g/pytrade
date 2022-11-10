f7814fb9b1814e9f924edf9145b8ddc2703322ab
sami-m-g <sami.mg@outlook.com>
2022-11-10 21:14:17 +0200

    Ammended tests to accomodate for new WilliamsParams fields

--- tests/test_williams.py
+++ tests/test_williams.py
@@ -16,37 +16,37 @@ class TestWilliams(unittest.TestCase):
     def test_get_status_sell(self) -> None:
         rows = [[2, 1, 1.9], [2, 1, 1.7]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsStatus.SELL.name, williams.get_status())
     
     def test_get_status_buy(self) -> None:
         rows = [[2, 1, 1.1], [2, 1, 1.5]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsStatus.BUY.name, williams.get_status())
 
     def test_get_status_gray(self) -> None:
         rows = [[2, 1, 1.3], [2, 1, 1.5]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsStatus.GRAY.name, williams.get_status())
 
     def test_get_position_overbought(self) -> None:
         rows = [[2, 1, 1.9]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsPosition.OVERBOUGHT.name, williams.get_position())
 
     def test_get_position_oversold(self) -> None:
         rows = [[2, 1, 1.1]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsPosition.OVERSOLD.name, williams.get_position())
 
     def test_get_position_middle(self) -> None:
         rows = [[2, 1, 1.5]]
         data = self.get_data(rows)
-        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0))
+        williams = Williams(data, WilliamsParams(1, -20, -80, WilliamsType.SHORT, 0, 0))
         self.assertEqual(WilliamsPosition.MIDDLE.name, williams.get_position())
 
 
